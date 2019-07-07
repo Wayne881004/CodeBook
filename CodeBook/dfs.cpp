8d7392cd49aba8fd1de6b7d8de7865eb22c20c0d
Wayne881004 <43371931+Wayne881004@users.noreply.github.com>
2019-07-08 01:59:25 +0800

    Create dfs.cpp

---
+++ CodeBook/dfs.cpp
@@ -0,0 +1,20 @@
+void dfs visit  (int i , int len){
+  for(int j = 0 ; j < n ; j++){
+      if(a[i][j]){
+          if(a[i][j]){
+            a[i][j] = a[j][i] = 0;
+            dfs_visit(j,len+1);
+            a[i][j] = a[j][i] = 1;
+          }
+  }
+  if(len > best) best = len;
+}
+void dfs(){
+  int i;
+  best = 0 ;
+  for(int i = 0 ; i < n ; i++){
+    dfs_visit(i,0);
+  }
+  cout << best << endl;
+}
+  
