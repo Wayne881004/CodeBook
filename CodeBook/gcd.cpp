2f047b14167c705684151ae32dc69e51fab87226
Wayne881004 <43371931+Wayne881004@users.noreply.github.com>
2019-07-08 01:34:16 +0800

    Update gcd.cpp

--- CodeBook/gcd.cpp
+++ CodeBook/gcd.cpp
@@ -6,3 +6,9 @@ int gcd(int a, int b)
 	return a;
 }
 // gcd(a,b) ⋅ lcm(a,b) = ab 
+int extgcd(int a,int b,int &x,int &y){
+    int d=a;
+    if(b){d=extgcd(b,a%b,y,x),y-=(a/b)*x;}
+    else x=1,y=0;
+    return d;
+}//ax+by=1 ax同餘 1 mod b
